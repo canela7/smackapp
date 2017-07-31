d347b45d92a49d6ee616bcb8eb65a859aa0925ab
canela7 <canelabrian123@gmail.com>
2017-07-30 17:22:38 -0700

    SWReveal animation added
    added swreveal component to the project to implement easy slide in and
    out tap in the option meanBtn

---
+++ SmackChat/Controller/ChannelVC.swift
@@ -0,0 +1,22 @@
+//
+//  ChannelVC.swift
+//  SmackChat
+//
+//  Created by Brian Canela on 7/30/17.
+//  Copyright © 2017 CanelaApps. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        
+    }
+
+  
+
+}
---
+++ SmackChat/Controller/ChatVC.swift
@@ -0,0 +1,28 @@
+//
+//  ChatVC.swift
+//  SmackChat
+//
+//  Created by Brian Canela on 7/30/17.
+//  Copyright © 2017 CanelaApps. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    //Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+       
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+
+   
+
+}
